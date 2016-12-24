cf2b7031dbea4d24cd06e6bc1d375dfd54e74350
Shaform <shaform@gmail.com>
2016-12-24 13:24:30 +0800

    improve cache & add docstrings

--- generate.py
+++ generate.py
@@ -2,23 +2,32 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Generate json file for top Python 2 only packages on GitHub"""
 
 import argparse
 import json
-import sys
 
 import utils
 
+RESULT_PATH = 'result.json'
+OVERRIDE_PATH = 'overrides.json'
+CACHE_PATH = 'cache.data'
+
 
 def print_progress(done, total, msg=''):
+    """Print progress
+
+    :param done: how many tasks have been done
+    :param msg: show text message along with the progress
+    """
     percent = done / total * 100
     print('\r', end='')
     k = int(percent // 5)
@@ -28,10 +37,16 @@ def print_progress(done, total, msg=''):
           end='')
 
 
-def generate(outfile, num_package):
+def generate(num_package, cache, overrides):
+    """Generate top package list that are Python 2 only
+
+    :param num_package: maximum number of packages to list
+    """
+
     top_packages = utils.get_top_packages()
-    py2_packages = utils.get_py2_packages(top_packages)
-    github_packages = utils.get_github_packages(py2_packages)
+    py2_packages_with_info = utils.get_py2_packages(top_packages, cache,
+                                                    overrides)
+    github_packages = utils.get_github_packages(py2_packages_with_info)
 
     top_py2_packages = []
     print_progress(0, num_package)
@@ -46,18 +61,32 @@ def generate(outfile, num_package):
             break
     print()
 
+    # sort by GitHub star instead of PyPI counts
     top_py2_packages.sort(key=lambda p: (-p['stars'], -p['downloads']))
     for package in top_py2_packages:
         print(package['stars'], package['name'], package['url'])
 
-    with open(outfile, 'w', encoding='utf8') as f:
-        json.dump(top_py2_packages, f)
+    return top_py2_packages
+
+
+def main():
+    """Generate Python 2 only package list."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--num-package', type=int, default=100)
+    parser.add_argument('-o', '--outfile', default=RESULT_PATH)
+    parser.add_argument('-c', '--cache-dir', default=CACHE_PATH)
+    parser.add_argument('--overrides', default=OVERRIDE_PATH)
+    args = parser.parse_args()
+
+    cache = utils.get_cache(args.cache_dir)
+    overrides = utils.get_overrides(args.overrides)
+    top_py2_packages = generate(args.num_package,
+                                cache=cache,
+                                overrides=overrides)
+
+    with open(args.outfile, 'w', encoding='utf8') as out:
+        json.dump(top_py2_packages, out)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--num-package', type=int, default=100)
-    parser.add_argument('-o', '--outfile', default='result.json')
-    args = parser.parse_args()
-
-    generate(args.outfile, args.num_package)
+    main()
--- utils.py
+++ utils.py
@@ -2,21 +2,22 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Utils to get data from PyPI, GitHub, etc."""
 
 import json
-import os
 import re
 import xmlrpc
 
 import caniusepython3
+import diskcache
 import lxml.html
 import requests
 
@@ -26,44 +27,44 @@ OVERRIDE_PATH = 'overrides.json'
 CACHE_PATH = 'cache.data'
 
 
-def get_overrides():
-    with open(OVERRIDE_PATH) as f:
-        packages = json.load(f)
+def get_overrides(path):
+    """Load overrides from json file."""
+    with open(path) as infile:
+        packages = json.load(infile)
     return packages
 
 
-def check_overrides(name, overrides=get_overrides()):
+def check_overrides(name, overrides):
+    """Check whether a package has been manually overridden.
+
+    :param name: the package name
+    :param overrides: the override dictionary
+    """
+
+    if overrides is None:
+        return None
+
     if name in overrides:
         return overrides[name]
 
+    # no need to list packages that contain only static files
     if name.startswith('XStatic'):
         return []
 
     return None
 
 
-def get_cache():
-    if os.path.exists(CACHE_PATH):
-        with open(CACHE_PATH) as f:
-            cache = {l.strip(): [] for l in f}
-    else:
-        cache = {}
-    return cache
-
-
-def check_cache(name, cache=get_cache()):
-    if name in cache:
-        return cache[name]
-
-    return None
-
-
-def write_cache(name):
-    with open(CACHE_PATH, 'a') as f:
-        f.write(name + '\n')
+def get_cache(path):
+    """Get cache dictionary"""
+    return diskcache.Cache(path)
 
 
 def get_github_stars(user, name):
+    """Crawl GitHub to obtain stars of a given repository.
+
+    :param user: username
+    :param name: name of the repository
+    """
     url = '{}/{}/{}'.format(GITHUB_URL, user, name)
     response = requests.get(url)
     if response.status_code == 200:
@@ -79,90 +80,136 @@ def get_github_stars(user, name):
 
 
 def get_top_packages():
+    """Get top packages from PyPI"""
     client = xmlrpc.client.ServerProxy(PYPI_URL)
     for name, downloads in client.top_packages():
         yield {'name': name, 'downloads': downloads}
 
 
-def get_github_packages(packages, check_python2=True):
-    session = requests.Session()
-    github_pattern = re.compile(
-        r'https?://github.com/([^/]+)/([A-Za-z0-9_.-]+)')
+GITHUB_PATTERN = re.compile(r'https?://github.com/([^/]+)/([A-Za-z0-9_.-]+)')
 
-    for package in packages:
+
+def get_github_info(package_name, package_info):
+    """Get GitHub info from package name."""
+
+    # check if homepage is a GitHub repository
+    home_page = package_info['info']['home_page']
+    if home_page:
+        matched = GITHUB_PATTERN.match(home_page)
+
+        if matched:
+            return matched.groups()
+
+    else:
+        desc = package_info['info']['description']
+        if desc:
+            for user, name in GITHUB_PATTERN.findall(desc):
+                if name == package_name:
+                    return user, name
+    return None, None
+
+
+def get_package_info(package_name, session):
+    """Fetch package information from PyPI."""
+    url = '{}/{}/json'.format(PYPI_URL, package_name)
+    response = session.get(url)
+    if response.status_code == 200:
+        return response.json()
+
+    return None
+
+
+def get_github_packages(packages_with_info, cache=None):
+    """Get packages that have GitHub repositories.
+
+    :param packages_with_info: an iterable of packages with package_info
+    """
+
+    # create a temporary cache that discards all info
+    if cache is None:
+        cache = {}
+
+    for package, package_info in packages_with_info:
+        # process only if it does not already have GitHub info
         if 'github_user' not in package:
-            url = '{}/{}/json'.format(PYPI_URL, package['name'])
-            response = session.get(url)
-            if response.status_code == 200:
-                # check github
-                package_info = response.json()
-                home_page = package_info['info']['home_page']
-                desc = package_info['info']['description']
+            user, name = get_github_info(package['name'], package_info)
+        else:
+            user, name = package['github_user'], package['github_name']
 
-                if home_page:
-                    m = github_pattern.match(home_page)
-                else:
-                    m = None
+        if user and name:
+            # store GitHub info
+            url = 'https://github.com/{}/{}'.format(user, name)
+            cache[package['name']] = (user, name, url)
 
-                if m:
-                    user, name = m.groups()
-                    package['github_user'] = user
-                    package['github_name'] = name
-
-                elif desc:
-                    package_name = package['name']
-                    for user, name in github_pattern.findall(desc):
-                        if name == package_name:
-                            package['github_user'] = user
-                            package['github_name'] = name
-                            break
-
-        if 'github_user' in package:
-            # only keep Python 2 versions
-            if check_python2:
-                skip = False
-                releases = package_info['releases']
-                for versions in releases.values():
-                    for version in versions:
-                        python_version = version['python_version']
-                        if python_version == 'py2.py3' or python_version.startswith(
-                                '3.'):
-                            skip = True
-                            break
-                    if skip:
-                        break
-
-                if skip:
-                    continue
-
-            # get stars
-            user = package['github_user']
-            name = package['github_name']
             stars = get_github_stars(user, name)
             if stars is not None:
-                package['stars'] = stars
-                package['url'] = 'https://github.com/{}/{}'.format(user, name)
-                yield package
+                package_with_github = {
+                    'name': package['name'],
+                    'downloads': package['downloads'],
+                    'github_user': user,
+                    'github_name': name,
+                    'stars': stars,
+                    'url': url,
+                }
+
+                yield package_with_github
 
 
-def get_py2_packages(packages):
+def is_python3_enabled(package_info):
+    """Check if a package is Python 3 enabled by released data."""
+    releases = package_info['releases']
+    for versions in releases.values():
+        for version in versions:
+            python_version = version['python_version']
+            if python_version == 'py2.py3' or python_version.startswith('3.'):
+                return True
+    return False
+
+
+def get_py2_packages(packages, cache=None, overrides=None):
+    """Get Python 2 only packages from package list
+
+    :param packages: an iterable of packages
+    :param cache: the cache dictionary to store package info
+    """
+
+    # create a temporary cache that discards all info
+    if cache is None:
+        cache = {}
+
+    session = requests.Session()
     for package in packages:
         name = package['name']
 
         # check if overridden as Python 3
-        override_status = check_overrides(name)
+        override_status = check_overrides(name, overrides=overrides)
         if override_status is None:
             # check if cached as Python 3
-            override_status = check_cache(name)
+            override_status = cache.get(name)
 
-        if override_status is None:
-            if not caniusepython3.check(projects=[name]):
-                yield package
+        if override_status is not None:
+            # only yield Python 2 packages
+            if len(override_status) == 3:
+                github_user, github_name, url = override_status
+                downloads = package['downloads']
 
-            else:
-                write_cache(name)
-        elif len(override_status) == 3:
-            package['github_user'] = override_status[0]
-            package['github_name'] = override_status[1]
-            package['url'] = override_status[2]
-            yield package
+                package_with_github = {
+                    'name': name,
+                    'downloads': downloads,
+                    'github_user': github_user,
+                    'github_name': github_name,
+                    'url': url,
+                }
+                yield (package_with_github, None)
+
+        else:
+            if caniusepython3.check(projects=[name]):
+                cache[name] = ()
+                continue
+
+            package_info = get_package_info(name, session)
+            if is_python3_enabled(package_info):
+                cache[name] = ()
+                continue
+
+            yield (package, package_info)
